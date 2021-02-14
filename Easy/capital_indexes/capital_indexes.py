e44bfc3fac06c5291639025be8f2ad53675bb581
Kevin <kevinromerooviedo@gmail.com>
2021-02-13 22:31:52 -0600

    Removed unnecessary comments in capital_indexes

--- Easy/capital_indexes/capital_indexes.py
+++ Easy/capital_indexes/capital_indexes.py
@@ -1,11 +1,3 @@
-# ALGORITHM
-# Import NumPy library
-# 1. Split the string into a characters array
-# 2. Iterate through the array, check if character is uppercase, and store
-#    character array index inside a new array
-# 3. Return the list
-
-# CODE
 def capital_indexes(string):
     char_array = [ch for ch in string]
     index_array = []
@@ -13,4 +5,4 @@ def capital_indexes(string):
         if (char_array[x].isupper()):
             index_array.append(x)
     return index_array
-print(capital_indexes("HeLlO"))
+print(capital_indexes("HeLlO")) # Returns "HLO"
